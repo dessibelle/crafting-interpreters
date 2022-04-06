2c96685c20ffc20c88b4fd78ece769913c1f9ca9
Simon Fransson <simon.fransson@funnel.io>
2022-04-06 10:12:13 +0200

    improve GenerateAst with visitor pattern

--- jlox/com/craftinginterpreters/tool/GenerateAst.java
+++ jlox/com/craftinginterpreters/tool/GenerateAst.java
@@ -36,6 +36,13 @@ public class GenerateAst {
 
     writer.println("    }");
 
+    // Visitor pattern.
+    writer.println();
+    writer.println("    @Override");
+    writer.println("    <R> R accept(Visitor<R> visitor) {");
+    writer.println("      return visitor.visit" + className + baseName + "(this);");
+    writer.println("    }");
+
     // Fields.
     writer.println();
     for (String field : fields) {
@@ -43,6 +50,18 @@ public class GenerateAst {
     }
 
     writer.println("  }");
+  }
+
+  private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+    writer.println("  interface Visitor<R> {");
+
+    for (String type : types) {
+      String typeName = type.split(":")[0].trim();
+      writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+    }
+
+    writer.println("  }");
+    writer.println();
   }
 
   private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
@@ -55,6 +74,8 @@ public class GenerateAst {
     writer.println();
     writer.println("abstract class " + baseName + " {");
 
+    defineVisitor(writer, baseName, types);
+
     // The AST classes.
     for (String type : types) {
       String className = type.split(":")[0].trim();
@@ -62,7 +83,12 @@ public class GenerateAst {
       defineType(writer, baseName, className, fields);
     }
 
+    // The base accept() method.
+    writer.println();
+    writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+
     writer.println("}");
+
     writer.close();
   }
 
